ac5cd0a5a2b6970d0f41a81e509c6721c0b168db
blackbird17922se <blackbird17922@gmail.com>
2021-10-27 16:26:24 -0500

    heroku 1

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongo_uri:
+    // 'mongodb://mintic:MinTic2021@clustermau-shard-00-00.t6imf.mongodb.net:27017,clustermau-shard-00-01.t6imf.mongodb.net:27017,clustermau-shard-00-02.t6imf.mongodb.net:27017/5storeapp?ssl=true&replicaSet=atlas-8snwti-shard-0&authSource=admin&retryWrites=true&w=majority'
+    'mongodb://127.0.0.1:27017/5Store'
+}
--- index.js
+++ index.js
@@ -7,12 +7,17 @@ import router from './routes/post.js';
 import Rproducto from './routes/Rproducto.js';
 import Rventa from './routes/Rventa.js';
 
+import keys from "./config/keys.js";
+import path from "path/posix";
+
 import cors from 'cors';
 // const bodyparser = require("body-parser");
 // const router = require('./routes/post.js')
 
-const URL = 'mongodb://mintic:MinTic2021@clustermau-shard-00-00.t6imf.mongodb.net:27017,clustermau-shard-00-01.t6imf.mongodb.net:27017,clustermau-shard-00-02.t6imf.mongodb.net:27017/5storeapp?ssl=true&replicaSet=atlas-8snwti-shard-0&authSource=admin&retryWrites=true&w=majority';
-mongoose.connect(URL);
+// const URL = 'mongodb://mintic:MinTic2021@clustermau-shard-00-00.t6imf.mongodb.net:27017,clustermau-shard-00-01.t6imf.mongodb.net:27017,clustermau-shard-00-02.t6imf.mongodb.net:27017/5storeapp?ssl=true&replicaSet=atlas-8snwti-shard-0&authSource=admin&retryWrites=true&w=majority';
+// const URL = 'mongodb://mintic:MinTic2021@clustermau-shard-00-00.t6imf.mongodb.net:27017,clustermau-shard-00-01.t6imf.mongodb.net:27017,clustermau-shard-00-02.t6imf.mongodb.net:27017/5storeapp?ssl=true&replicaSet=atlas-8snwti-shard-0&authSource=admin&retryWrites=true&w=majority';
+// mongoose.connect(URL);
+mongoose.connect(keys.mongo_uri)
 
 /* 2. cuando lo invoque entonces me retorna un objeto express que lo guadare en : */
 const appExpress = express();
@@ -24,6 +29,14 @@ appExpress.use(cors());
 appExpress.use('/posts', router);
 appExpress.use('/Rproducto', Rproducto);
 appExpress.use('/Rventa', Rventa);
+
+if(process.env.NODE_ENV === "production"){
+    appExpress.use(express.static("client/build"));
+    // const pth = path;
+    appExpress.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 appExpress.get("/", (req, resp) => {
     resp.send({Proyecto: "5store"})
